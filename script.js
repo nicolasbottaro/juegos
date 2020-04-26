fc8203590acb95435856dc7d114894da72b36497
nicolasbottaro <64370092+nicolasbottaro@users.noreply.github.com>
2020-04-26 16:06:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,42 @@
-const button = document.querySelector('.btn1')
-const button1 = document.querySelector('.btn2')
-const texto = document.querySelector('p')
-const body = document.querySelector('body')
-const colors = ['white','yellow', 'orange', 'red', 'black']
-const colorsCast = ['Blanco','Amarillo', 'Naranja', 'Rojo', 'Negro']
+const button1 = document.querySelector('.btn1')
+const button2 = document.querySelector('.btn2')
+const button3 = document.querySelector('.btn3')
+const button4 = document.querySelector('.btn4')
+const puntaje = document.querySelector('h2')
+const quedan = document.querySelector('p')
 var i = 0
+let activo = 1
 
-body.style.backgroundColor = 'white'
-button.addEventListener('click', changeBackground)
-button1.addEventListener('click', changeBackground1)
+button1.addEventListener('click', sumarPuntaje);
+button2.addEventListener('click', sumarPuntaje)
+button3.addEventListener('click', sumarPuntaje)
+button4.addEventListener('click', sumarPuntaje)
 
-function changeBackground(){
 
-if (i < 4) {
-   i++;
-} else
-   i=0;
-   body.style.backgroundColor = colors[i]
-   texto.innerHTML = colorsCast[i]
-}
+let empezar = setInterval(botonActivo, 2500);
+setTimeout(Apagar,20000)
 
-function changeBackground1(){
-
-   i=i-1;
-   if (i>-1){
-   body.style.backgroundColor = colors[i]
-   texto.innerHTML = colorsCast[i]} else i=4; body.style.backgroundColor = colors[i]
-   texto.innerHTML = colorsCast[i]
+function Apagar() {
+  clearInterval(empezar);
+  puntaje.innerHTML = 'Termino el tiempo. Tu puntaje fue de ' + i + ' puntos';
+  quedan.innerHTML = '';
 }
 
 
+function botonActivo(){
+  activo = Math.round(Math.random()*3) +1 ;
+  document.querySelector('.btn'+activo).classList.add("activo");
+  setTimeout(botonNoActivo,700)
+}
+
+function botonNoActivo(){
+  document.querySelector('.btn'+activo).classList.remove("activo")
+}
+
+
+function sumarPuntaje(){
+  if (this.classList.contains('activo') === true) {
+  i++;
+  puntaje.innerHTML = 'Puntaje: ' + i;
+  }
+}
